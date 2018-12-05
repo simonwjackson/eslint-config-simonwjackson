8bf749094b6df728142b0bb99a2595aac04c463f
Simon W. Jackson <simon@simonwjackson.com>
2018-12-04 21:53:19 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-multiple-empty-lines": ["error", {
+            "max": 1,
+            "maxBOF": 1
+        }],
+        "brace-style": ["error", "stroustrup"],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
