5227981fe9ca0971bc96b19d77014e1af8d57e4d
Simon W. Jackson <simon@simonwjackson.com>
2019-01-07 13:24:10 -0600

    add curly rule

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
     "rules": {
         'space-infix-ops': "error",
         "object-curly-spacing": ["error", "always"],
+        'curly': ["error", "multi", "consistent"],
         "quote-props": ["error", "as-needed"],
         "no-multiple-empty-lines": ["error", {
             "max": 1,
